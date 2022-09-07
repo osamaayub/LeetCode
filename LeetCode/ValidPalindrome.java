1f538b33581a7f9fa3b66d8e21b62332ae513f42
Osama ayub <ayubosama416@gmail.com>
2022-09-07 16:11:39 +0500

    Valid Palindrome Leetcode solution

---
+++ LeetCode/ValidPalindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^a-zA-Z0-9]","");
+        s=s.toLowerCase();
+        
+        int start=0,end=s.length()-1;
+    while(start<end)
+    {
+        if(s.charAt(start)!=s.charAt(end))
+           {
+            return false;
+          
+           }
+             start++;
+        end--;
+    }
+           return true;
+        
+    }
+}
